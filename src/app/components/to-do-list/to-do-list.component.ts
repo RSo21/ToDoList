8e51259d3fc595817ec0d6fb76643abad958926a
R <roksana_sobczak@wp.pl>
2020-06-30 21:12:57 +0200

    add select element and angular material dependency

--- src/app/components/to-do-list/to-do-list.component.ts
+++ src/app/components/to-do-list/to-do-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 //import { Todo } from '../../interfaces/todo';
+import { Status } from '../../interfaces/status';
+import { Priority } from '../../interfaces/priority';
 
 @Component({
   selector: 'to-do-list',
@@ -10,39 +12,67 @@ export class ToDoListComponent implements OnInit {
   todos: object[];
   todoTitle: string;
   todoId: number;
+  todoStatuses: Status[];
+  todoDescription: string;
+  todoDateTime: string;
+  todoPriorities: Priority[];
+
+  selectedValue: string;
+  selectedValuePrior: string;
 
   constructor() { }
 
   ngOnInit() {
     this.todoTitle = '';
     this.todoId = 4;
+    this.todoStatuses = [
+      {value: 'to do', viewValue: 'to do'},
+      {value: 'in progress', viewValue: 'in progress'},
+      {value: 'done', viewValue: 'done'},
+    ];
+    this.todoDescription = '';
+    this.todoDateTime = '';
+    this.todoPriorities = [
+      {value: 'low', viewValue: 'low'},
+      {value: 'medium', viewValue: 'medium'},
+      {value: 'high', viewValue: 'high'},
+    ];
     this.todos = [
       {
         'id': 1,
         'title': 'Check what TypeScript is',
+        'selectedValue': 'to do',
       },
       {
         'id': 2,
         'title': 'Go to sleep',
+        'selectedValue': '',
       },
       {
         'id': 3,
         'title': 'Save the world',
+        'selectedValue': '',
       },
     ];
   }
 
   addTodo(): void {
-    if (this.todoTitle.trim().length === 0) {
+    if (this.todoTitle.trim().length === 0 
+    && this.selectedValue.trim().length === 0
+    && this.selectedValuePrior.trim().length === 0) {
       return;
     }
 
     this.todos.push({
       id: this.todoId,
       title: this.todoTitle,
+      status: this.selectedValue,
+      priority: this.selectedValuePrior,
     })
 
     this.todoTitle = '';
+    this.selectedValue= '';
+    this.selectedValuePrior= '',
     this.todoId++;
   }
 }
